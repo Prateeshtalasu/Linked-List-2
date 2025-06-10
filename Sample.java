23401dd541ee8d5c15b973e4bda7c0ee0080d5db
Prateeshtalasu <prateeshtalasu1999@gmail.com>
2025-06-09 22:08:49 -0700

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,37 @@
 
 
 // Your code here along with comments explaining your approach
+class BSTIterator {
+  Stack<TreeNode> stack;
+
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack <> ();
+        while(root!=null){
+            stack.push(root);
+            root = root.left;
+        }
+        
+    }
+    public int next() {
+        // Pop the smallest node (top of stack)
+        TreeNode node = stack.pop();
+    
+    // If it has a right child, go left as far as possible from there
+        TreeNode right = node.right;
+        while (right != null) {
+            stack.push(right);
+            right = right.left;
+        }
+
+        return node.val;
+    }
+    
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
+        
+    }
+}
+///
+/// 
